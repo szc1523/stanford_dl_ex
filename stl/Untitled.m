6b710479d6d623d0687b26560ca26a98d2084d26
szc1523 <szc1523@163.com>
2015-08-20 18:11:00 +0800

    rewrite softmax, the derivative_check in minFun seems to have some bugs in it, grad_check is a lot more useful

--- stl/Untitled.m
+++ stl/Untitled.m
@@ -2,7 +2,7 @@ addpath(genpath('..'))
 
 numClasses  = 5; % doing 5-class digit recognition
 % initialize softmax weights randomly
-randTheta2 = randn(numClasses-1, featureSize)*0.01;  % 1/sqrt(params.n);
+randTheta2 = randn(numClasses, featureSize)*0.01;  % 1/sqrt(params.n);
 randTheta2 = randTheta2 ./ repmat(sqrt(sum(randTheta2.^2,2)), 1, size(randTheta2,2)); 
 randTheta2 = randTheta2';
 randTheta2 = randTheta2(:);
@@ -15,8 +15,10 @@ options.MaxIter = 300;
 
 % optimize
 %%% YOUR CODE HERE %%%
-theta(:) = minFunc(@softmax_regression_vec, randTheta2, options, ...
+theta(:) = derivativeCheck(@softmaxcost, randTheta2, 1, 1, ...
   trainFeatures, trainLabels);
-theta=[theta, zeros(n,1)]; % expand theta to include the last class.
-[~,train_pred] = max(theta'*trainFeatures, [], 1);
-[~,pred] = max(theta'*testFeatures, [], 1);
+
+
+% theta=[theta, zeros(n,1)]; % expand theta to include the last class.
+% [~,train_pred] = max(theta'*trainFeatures, [], 1);
+% [~,pred] = max(theta'*testFeatures, [], 1);
--- stl/softmaxcost.m
+++ stl/softmaxcost.m
@@ -1,4 +1,4 @@
-function [ output_args ] = softmaxcost( input_args )
+function [f, g, preds] = softmaxcost( theta, X, y, pred)
 % softmax This function is the costfunction of softmax. its different from
 % the one in ex1 folder, in that it has full num_classes instead of
 % num_classes - 1. Although the latter one saves computation time. it
@@ -8,12 +8,43 @@ function [ output_args ] = softmaxcost( input_args )
   %   theta - A vector containing the parameter values to optimize.
   %       In minFunc, theta is reshaped to a long vector.  So we need to
   %       resize it to an n-by-num_classes matrix.
-
   %
   %   X - The examples stored in a matrix.  
   %       X(i,j) is the i'th coordinate of the j'th example.
   %   y - The label for each example.  y(j) is the j'th example's label.
 
+if ~exist('pred','var')
+  pred = false;
+end;
+m=size(X,2);
+n=size(X,1);
+
+% theta is a vector;  need to reshape to n x num_classes.
+theta=reshape(theta, n, []);
+num_classes=size(theta,2);
+
+%% forward propagation
+h = exp(theta'*X);  % no bias term
+probs = bsxfun(@rdivide, h, sum(h));
+
+%calculate cost
+I = sub2ind(size(probs), y, 1:m); 
+f = (-1/m)*sum(log(probs(I)));  
+
+% Makes predictions given probs and returns without backproagating errors.
+if pred
+    [~,preds] = max(probs,[],1);
+    preds = preds';
+    g = 0;
+    return;
+end;
+
+%% backpropagation
+ym = zeros(num_classes,m);  %ym is y matrix
+ym(I) = 1;
+
+g = (-1/m)*X*(ym-probs)';
+g = g(:);
 
 end
 
